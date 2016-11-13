2367c135f23ca86d1bf3e06bc546a6bf4fc88745
fredFreemanServer <fred.iafreeman@gmail.com>
2016-11-12 22:28:50 -0600

    things kinda work better

--- server.js
+++ server.js
@@ -20,14 +20,30 @@ var insertDocument = function(email, password, db, callback) {
   });
 };
 
-var findUser = function(email, password, db, callback) {
-   db.collection('restaurants').findOne( {
-   email: password,
+var findUser = function(_email, _password, db, callback) {
+  /*db.collection('users').find({}).toArray(function(err, docs){
+    assert.equal(err, null);
+    console.log("Found these");
+    console.log(docs);
+    callback(null);
+  });
+  */ 
+  db.collection('users').findOne( {
+   email : _email,
+   password : _password,
     } , function(err, doc){
         console.log("looked for user")
-        #return if the the (email -> password) was found back to the caller
-        callback( (doc != null) );
+        if(doc == null){
+          console.log("Found null");
+          callback(null);
+        }
+        else
+          console.log(doc.password);
+        
+        //return if the the (email -> password) was found back to the caller
+        callback(false);
    });
+   //*/
 };
 
 
@@ -41,20 +57,24 @@ io.on('connection', function(socket){
   socket.on('login', function (email, password) {
 		console.log('User is trying to login');
 		MongoClient.connect(url, function(err, db) {
-          assert.equal(null, err);
-          #call a function to find (email->password) entry was found and get a response
-          findUser(email, password, db, function(found) {
-              if(found == true){
-                socket.emit('login_status', 'VERIFIED');
-              }
-              else{
-                socket.emit('login_status', 'WRONG_PASSWORD');
-              }
-              db.close();
-          });
-        });
+      assert.equal(null, err);
+      //call a function to find (email->password) entry was found and get a response
+      findUser(email, password, db, function(found) {
+        if(found == true){
+          socket.emit('login_status', 'VERIFIED');
+        }
+        else{
+          socket.emit('login_status', 'WRONG_PASSWORD');
+        }
+        db.close();
+      });
+    });
 
   });
+
+  socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
 
   socket.on('register', function (email, password) {
   		console.log('User ', email, 'is regestrating');
